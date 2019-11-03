c1c35fa8535ff3a4aea344215721f12e1947b2e8
AmosMwangi <amosmwangi101@gmail.com>
2019-11-03 06:56:16 +0300

    made imports and methods with parameters

--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from './../search.service';
+import { Repo } from '../repo';
 
 @Component({
   selector: 'app-repo',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepoComponent implements OnInit {
 
-  constructor() { }
+  repo: Repo;
+  constructor(public repService: SearchService) {}
+
+  repoSearch(searchName){
+    this.repService.getRepos(searchName).then(
+      (result) => {
+        this.repo = this.repService.getRepo;
+        console.log(this.repo);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 
   ngOnInit() {
   }
