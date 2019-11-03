00988f32144f5ba017995940872792b9cfcc7392
AmosMwangi <amosmwangi101@gmail.com>
2019-11-03 05:26:43 +0300

    placed the api token in environments folder

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '0ab8db4d71990cb021b9459f1ed53cae0dbd5c2b',
 };
 
 /*
